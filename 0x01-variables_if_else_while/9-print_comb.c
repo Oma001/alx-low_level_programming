a257d3500aedf94fc7bff5eb4f61463156dc3563
0ma001 <123173079+Oma001@users.noreply.github.com>
2023-02-17 18:53:50 -0800

    My commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,21 +2,23 @@
 
 /**
  * main - main function
+ *
  * Return: Always 0
  */
+
 int main(void)
 {
-	int n;
+	int i;
 
-	for (n = 48; n  < 58; n++)
+	for (i = 0 ; i  < 10 ; i++)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(i + '0');
+		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
-
 		}
+	}	
 	putchar('\n');
 	return (0);
 }
