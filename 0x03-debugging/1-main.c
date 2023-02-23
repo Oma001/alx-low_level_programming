6318196097fc38133b876326c7e402f46fa0a1ff
0ma001 <123173079+Oma001@users.noreply.github.com>
2023-02-22 20:03:32 -0800

    my commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinte loop
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	print("infinite loop incoming :(\n");
+	i = 0;
+	/*while (i < 10)*/
+	/*{*/
+	/*      putchar(i);*/
+	/*}*/
+	printf("infinite loop avoided! \\0/\n");
+	return (0);
+}
