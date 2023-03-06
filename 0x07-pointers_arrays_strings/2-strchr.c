0862903e30a0763bbdd02fb3eed1572b9998b033
0ma001 <123173079+Oma001@users.noreply.github.com>
2023-03-06 06:42:12 -0800

    my commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main"
+
+/**
+ * _strchr _ Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			retrun (s +i);
+	}
+
+	return (NULL);
+}
