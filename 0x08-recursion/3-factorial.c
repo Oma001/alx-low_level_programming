79b2044e15fe96d358184296a0d1a0d879a256b5
0ma001 <123173079+Oma001@users.noreply.github.com>
2023-03-08 18:32:23 -0800

    my commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * factroial - calculate the factorial of a number
+ * factorial - calculate the factorial of a number
  * @n: the number to calculate the factorial
  *
  * Return: an integer
